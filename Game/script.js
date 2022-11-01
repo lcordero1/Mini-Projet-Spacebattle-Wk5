6d7109941f57c6e13f09668767f6cf84f5779eae
Lariana C01~ <cordero.lariana@gmail.com>
2022-11-01 07:57:51 -0400

    Edit JavaScript

--- Game/script.js
+++ Game/script.js
@@ -2,6 +2,8 @@
 /*
 You attack the first alien ship
 
+if EarthStar
+
 If the ship survives, it attacks you
 
 If you survive, you attack the ship again
@@ -46,113 +48,81 @@ const mainPlayer1El = document.getElementById('characther')
 const gameEl = document.getElementById('enemy')
 const scoreEl = document.querySelector('score')
 const computer = document.querySelector('computer')
+const startContainer = document.querySelector('start-container')
 
-const game = 
-
-{
+const game = {
     players: [],
     start: function () {
         startContainer.classList.toggle("hide")
         gameContainer.classList.toggle("hide")
     },
-}
-//Creating the main player: the spaceship
-class EarthStar {
-    construtor(hp, firepower, accuracy){
-        this.hp = 20;
-        this.firepower = 5;
-        this.accuracy = .7;
-    }
-     
-    attack(){
-        console.log('Die! You vile creature!');
-    }
-    lose(){
-        console.log('Oh no! Ill get you next time!');
+    //battle
+    battle = (EarthStar) => {
 
-    }
-    win(){
-        console.log('Planet Earth is saved! Hip-hip-HOORAY!');
-    }
-}
-console.log(EarthStar)
+        outcome()
 
-
-
-class Enemy {
-    constructor(hpEne, fiepowerEne, accuracyEne){
-        this.hpEne = Math.round((Math.random() * (6 - 3)) + 3),
-        this.firepowerEne = Math.round((Math.random() * (4 - 2)) + 2),
-        this.accuracyEne = (Math.random() * (.8 - .6)) + .6 
-    }
-
-    attackShip(EarthStar) {
-        let ranInteger = Math.random();
-        console.log(`Enemy accuracy is ${ranNum}`);
-
-        if (ranInteger < this.accuracy) {
-            console.log(`${mainPlayer} has been hit!`);
-            EarthStar.hp = target.hp - this.firepowerEne;
-            console.log(`${EarthStar} hp level is now ${target.hp}`);
-            if (target.hp <= 0) {
-                console.log(`DEATH TO HUMANS!`);
+        while (alienCount > 0) {
+            if (alien.hpEne < 1) {
+                alienCount--
+            }
         }
-        const alien = new Enemy();
-    }
-}
-
-//creating group of aliens(6)
-
-
-alienCount = 6;
 
 
 
-// const alien1 = new Enemy(hpEne.getRndInteger(3, 6),firePowerEne.getRndInteger(2, 4), accuracyEne.Math.floor(Math.random))
-// const alien2 = new Enemy(hpEne.getRndInteger(3, 6),firePowerEne.getRndInteger(2, 4), accuracyEne.Math.floor(Math.random))
-// const alien3 = new Enemy(hpEne.getRndInteger(3, 6),firePowerEne.getRndInteger(2, 4), accuracyEne.Math.floor(Math.random))
-// const alien4 = new Enemy(hpEne.getRndInteger(3, 6),firePowerEne.getRndInteger(2, 4), accuracyEne.Math.floor(Math.random))
-// const alien5 = new Enemy(hpEne.getRndInteger(3, 6),firePowerEne.getRndInteger(2, 4), accuracyEne.Math.floor(Math.random))
-// const alien6 = new Enemy(hpEne.getRndInteger(3, 6),firePowerEne.getRndInteger(2, 4), accuracyEne.Math.floor(Math.random))
+        //Creating the main player: the spaceship
+        class EarthStar {
+            construtor(hp, firepower, accuracy) {
+                this.hp = 20;
+                this.firepower = 5;
+                this.accuracy = .7;
+            }
+
+            attack() {
+                console.log('Die! You vile creature!');
+            }
+            lose() {
+                console.log('Oh no! Ill get you next time!');
+
+            }
+            win() {
+                console.log('Planet Earth is saved! Hip-hip-HOORAY!');
+            }
+        }
+        console.log(EarthStar)
 
 
 
-//battle
+        class Enemy {
+            constructor(hpEne, fiepowerEne, accuracyEne) {
+                this.hpEne = Math.round((Math.random() * (6 - 3)) + 3),
+                    this.firepowerEne = Math.round((Math.random() * (4 - 2)) + 2),
+                    this.accuracyEne = (Math.random() * (.8 - .6)) + .6
+            }
 
-// const battle = 
+            attackShip(EarthStar) {
+                let ranInteger = Math.random();
+                console.log(`Enemy accuracy is ${ranInteger}`);
+
+                if (ranInteger < this.accuracy) {
+                    console.log(`${mainPlayer} has been hit!`);
+                    EarthStar.hp = target.hp - this.firepowerEne;
+                    console.log(`${EarthStar} hp level is now ${target.hp}`);
+                    if (target.hp <= 0) {
+                        console.log(`DEATH TO HUMANS!`);
+                    }
+                    const alien = new Enemy();
+                    //creating group of aliens(6)
+                    alienCount = 6;
+                }
+            }
+
+          
+
+
+           
 
 
 
-// function getRndInteger(min, max){
-//     return Math.floor(Math.random) 
-// }
-// let hpEne = getRndInteger(3, 6);
-// let firepowerEne = getRndInteger(2, 4)
-// let accuracyEne = math.floor(Math.random)
 
-//Enemy: which is the aliens
-// class Enemy{
-//     constructor(hpEne, firePowerEne, accuracyEne) {
-//         hull: Math.round((Math.random() * (6 - 3)) + 3),
-//         firepower: Math.round((Math.random() * (4 - 2)) + 2),
-//         accuracy: (Math.random() * (.8 - .6)) + .6
-//     } 
-//     
-// }
 
-// const alien1 = new Enemy
-// newEnemy(alien1){
-//   allEnemy(){
-//     return this.newEnemy
-//   }
-//   numberOfEnemy(){
-//       return this.newEnemy.length
-//   }
-// }
 
-// const leagueOfAliens = new Enemy()
-// league.newEnemy()
-// league.newEnemy()
-
-// console.log(leagueOfAliens.numberOf + 'Enemy')
-// console.log(leagueOfAliens.allEnemy)
